0d4a7afbd1ac7b2e726267711dd1cf004b6a5d98
Avi Aryan <avi.aryan123@gmail.com>
2017-11-10 12:50:21 +0530

    removing focus from map in detail view

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -14,6 +14,18 @@ window.initMap = () => {
         center: restaurant.latlng,
         scrollwheel: false
       });
+
+      // remove focus
+      // assuming map doesn't need focus
+      // https://stackoverflow.com/questions/30531075/
+      google.maps.event.addListener(self.map, "tilesloaded", function(){
+        [].slice.apply(document.querySelectorAll('#map a,button')).forEach(function(item) {
+          item.setAttribute('tabindex','-1');
+        });
+        // div for map satellite buttons
+        // button for right side zoom buttons
+      });
+
       fillBreadcrumb();
       DBHelper.mapMarkerForRestaurant(self.restaurant, self.map);
     }
